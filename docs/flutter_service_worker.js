f853cab197b51a9c0e08ae0a21fd7fe511a374c7
bert.gollnick <bert.gollnick@posteo.net>
2023-10-22 11:55:20 +0200

    bugfix images not shown

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -1039,8 +1039,8 @@ const RESOURCES = {"assets/AssetManifest.json": "85f36794deffebd9f4563f7804a54f0
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3eafd02a18c1debfbecf6428079c615f",
-"/": "3eafd02a18c1debfbecf6428079c615f",
+"index.html": "8a852be16986f2dc6114939ce27e48ab",
+"/": "8a852be16986f2dc6114939ce27e48ab",
 "main.dart.js": "3b546b947439941494c2ef9a63b7b504",
 "manifest.json": "b05a3411979535077ffeebd8e7a84b78",
 "version.json": "70e92e53b82782e46b06c6bc9bcec67d"};
