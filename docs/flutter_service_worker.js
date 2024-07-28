f376d4e47db776930372f162994fda14ae2f33d6
bert.gollnick <bert.gollnick@posteo.net>
2024-07-28 10:13:08 +0200

    link to calendly

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -1067,9 +1067,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "9c506b04c2413655e6497d800318b3e9
 "icons/Icon-512.png": "8b74f20fa10c344a7d083a876d199d3c",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "452cb953e53d11bbfb881143c297c78e",
-"/": "452cb953e53d11bbfb881143c297c78e",
-"main.dart.js": "9939e4ade7274beffd3c5b05add983a9",
+"index.html": "c57ee4422c5c4add840269fc138e99ae",
+"/": "c57ee4422c5c4add840269fc138e99ae",
+"main.dart.js": "8e5de7a40bd9f472eb8c6c061ad19211",
 "manifest.json": "b05a3411979535077ffeebd8e7a84b78",
 "version.json": "70e92e53b82782e46b06c6bc9bcec67d"};
 // The application shell files that are downloaded before a service worker can
