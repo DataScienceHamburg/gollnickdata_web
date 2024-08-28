3b0ab91917ecaa2a44f0616be3be7e781fc6aa76
bert.gollnick <bert.gollnick@posteo.net>
2024-08-28 17:41:00 +0200

    ui changes, Bremen as certified added

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -1334,9 +1334,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "04fae23162929bbc2707189ee5030442
 "icons/Icon-512.png": "8b74f20fa10c344a7d083a876d199d3c",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e860072a0ed2841cafdbe055dfa991b9",
-"/": "e860072a0ed2841cafdbe055dfa991b9",
-"main.dart.js": "51a61bdad08e24dfc7583614561a7cd0",
+"index.html": "faf4b460093dca766ee1e139ed330666",
+"/": "faf4b460093dca766ee1e139ed330666",
+"main.dart.js": "d9011abf289be27ccc44cef56de78a03",
 "manifest.json": "b05a3411979535077ffeebd8e7a84b78",
 "version.json": "70e92e53b82782e46b06c6bc9bcec67d"};
 // The application shell files that are downloaded before a service worker can
