681749fcd5d6ca0f6f93ba8fe6b0711288fe5864
bert.gollnick <bert.gollnick@posteo.net>
2024-11-01 09:39:12 +0100

    now without any cross links between pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -1352,9 +1352,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "46e4533cbb9ce46cf44472ff6112ad02
 "icons/Icon-512.png": "8b74f20fa10c344a7d083a876d199d3c",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "bf4c38e808bc733edc129668ff6ec885",
-"/": "bf4c38e808bc733edc129668ff6ec885",
-"main.dart.js": "d5d313edd1ebc848e3fabfe5a845a3bd",
+"index.html": "147284630272e59967d9282011cdf55c",
+"/": "147284630272e59967d9282011cdf55c",
+"main.dart.js": "e7d1e6d1ccdd79841222f68f84b26651",
 "manifest.json": "b05a3411979535077ffeebd8e7a84b78",
 "version.json": "70e92e53b82782e46b06c6bc9bcec67d"};
 // The application shell files that are downloaded before a service worker can
