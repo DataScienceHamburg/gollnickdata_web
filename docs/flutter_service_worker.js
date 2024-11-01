1e63471773bc2c10c3b041c58bc13e297df1b8a4
bert.gollnick <bert.gollnick@posteo.net>
2024-11-01 08:38:17 +0100

    bugfix: links provided as constants via position

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -1352,9 +1352,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "46e4533cbb9ce46cf44472ff6112ad02
 "icons/Icon-512.png": "8b74f20fa10c344a7d083a876d199d3c",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8fd1178bbfb65f8213ca47a2980640ee",
-"/": "8fd1178bbfb65f8213ca47a2980640ee",
-"main.dart.js": "eb26744b999ad9fc5a6dbf648a7719d4",
+"index.html": "511c0cd85107b438de3345eb8823f035",
+"/": "511c0cd85107b438de3345eb8823f035",
+"main.dart.js": "cb25b90186676e798022b0d98ce34697",
 "manifest.json": "b05a3411979535077ffeebd8e7a84b78",
 "version.json": "70e92e53b82782e46b06c6bc9bcec67d"};
 // The application shell files that are downloaded before a service worker can
