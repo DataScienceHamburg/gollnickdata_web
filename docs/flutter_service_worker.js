890ef20e62ed8407bb17060836692dc3cb64beb4
bert.gollnick <bert.gollnick@posteo.net>
2024-11-01 10:38:46 +0100

    without svg images

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -1352,9 +1352,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "46e4533cbb9ce46cf44472ff6112ad02
 "icons/Icon-512.png": "8b74f20fa10c344a7d083a876d199d3c",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "39e87c78218cb11e4368e140a86033ee",
-"/": "39e87c78218cb11e4368e140a86033ee",
-"main.dart.js": "0ab0843135a6e5a2af24f42d31716622",
+"index.html": "de78a1255a2d00caa0fce32512e56a6a",
+"/": "de78a1255a2d00caa0fce32512e56a6a",
+"main.dart.js": "ad19ad0d4e358713e96746f163933ee4",
 "manifest.json": "b05a3411979535077ffeebd8e7a84b78",
 "version.json": "70e92e53b82782e46b06c6bc9bcec67d"};
 // The application shell files that are downloaded before a service worker can
