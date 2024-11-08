764e1b8648b6bff4cfa128dc8b5afc3a3171d75e
bert.gollnick <bert.gollnick@posteo.net>
2024-11-08 11:24:45 +0100

    Jesko entfernt, Platz update

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -1352,9 +1352,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "46e4533cbb9ce46cf44472ff6112ad02
 "icons/Icon-512.png": "8b74f20fa10c344a7d083a876d199d3c",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b3af89f256fbfa3577af54557531e7a3",
-"/": "b3af89f256fbfa3577af54557531e7a3",
-"main.dart.js": "256dfe2f7687728d387a39cc04861042",
+"index.html": "03f58bec241570315ecdbcec8b91668a",
+"/": "03f58bec241570315ecdbcec8b91668a",
+"main.dart.js": "356a449a256400c2d4c93819bd844a8e",
 "manifest.json": "b05a3411979535077ffeebd8e7a84b78",
 "version.json": "70e92e53b82782e46b06c6bc9bcec67d"};
 // The application shell files that are downloaded before a service worker can
