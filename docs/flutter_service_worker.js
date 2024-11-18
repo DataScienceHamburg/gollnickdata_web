9da78dc57c495a23f476aae5f913e221876f0b8a
bert.gollnick <bert.gollnick@posteo.net>
2024-11-18 08:20:29 +0100

    Kursplätze geupdated

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -1352,9 +1352,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "46e4533cbb9ce46cf44472ff6112ad02
 "icons/Icon-512.png": "8b74f20fa10c344a7d083a876d199d3c",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "60fe0e19349a2f424920dd3d5193ccd1",
-"/": "60fe0e19349a2f424920dd3d5193ccd1",
-"main.dart.js": "58acd76e1a64a5005b5d2428f58d0071",
+"index.html": "b3b9319c2242cadb39c8003a34d22955",
+"/": "b3b9319c2242cadb39c8003a34d22955",
+"main.dart.js": "4f2d51fbf6c671bd2c249c0d961bf498",
 "manifest.json": "b05a3411979535077ffeebd8e7a84b78",
 "version.json": "70e92e53b82782e46b06c6bc9bcec67d"};
 // The application shell files that are downloaded before a service worker can
