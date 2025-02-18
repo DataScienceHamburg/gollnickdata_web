30f3ba9d7047a0c2ef22bf55ba5301a7f24e3e48
bert.gollnick <bert.gollnick@posteo.net>
2025-02-18 22:15:02 +0100

    RLP for AI-Eng added

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2706910630"
+    serviceWorkerVersion: "4138579200"
   }
 });
