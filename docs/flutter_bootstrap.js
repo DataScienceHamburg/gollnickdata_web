b62eec2cccd937f2d604b241ee41bea0880f66c4
Bert Gollnick <info@gollnickdata.de>
2025-05-07 15:45:23 +0200

    incl. VAT added

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2238130020"
+    serviceWorkerVersion: "2682436167"
   }
 });
