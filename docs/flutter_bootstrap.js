a3320348af3da6941a17499b6102e93cb9fe2338
Bert Gollnick <info@gollnickdata.de>
2025-05-12 08:05:08 +0200

    Kursmaterial bei KI Strategie wird nicht herausgegeben

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1859074996"
+    serviceWorkerVersion: "3813385543"
   }
 });
