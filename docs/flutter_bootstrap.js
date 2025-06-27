1d24e7604c38bb1d78e8ffe1ab7b13a8699da15e
Bert Gollnick <info@gollnickdata.de>
2025-06-27 09:50:02 +0200

    SEO improvements

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1581971705"
+    serviceWorkerVersion: "3264993615"
   }
 });
