5223dff5d974ed338a10ae8161818b3b66af02e3
Bert Gollnick <info@gollnickdata.de>
2025-06-27 09:54:32 +0200

    title tag slightly shortened

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3264993615"
+    serviceWorkerVersion: "1339451335"
   }
 });
