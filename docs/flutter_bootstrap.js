d47f66a1c94661a30230f27cdaa4c409021e4ffc
Bert Gollnick <info@gollnickdata.de>
2025-07-11 13:04:08 +0200

    course image and link updated

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2198800831"
+    serviceWorkerVersion: "3838512646"
   }
 });
