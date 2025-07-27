e4e55e6f0b7cb4a68afed0411645cb0549751621
Bert Gollnick <info@gollnickdata.de>
2025-07-27 18:28:36 +0200

    Azure AI updated

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "888356019"
+    serviceWorkerVersion: "91916505"
   }
 });
