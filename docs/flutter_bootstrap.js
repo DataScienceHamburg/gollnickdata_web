f8919c0d4aff9be2322a86c8bb27145680b08d0f
Bert Gollnick <info@gollnickdata.de>
2025-07-28 14:40:45 +0200

    AI Engineering für 2027 ergänzt

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1739435206"
+    serviceWorkerVersion: "3479555188"
   }
 });
